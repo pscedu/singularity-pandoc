888c3dd970bfbbcb60a7cfc7c00531d015eab721
halbakri <84340692+halbakri@users.noreply.github.com>
2021-07-27 11:16:34 -0400

    Create modulefile.lua

---
+++ 2.2.1/modulefile.lua
@@ -0,0 +1,33 @@
+--
+-- pandoc 2.2.1 modulefile
+--
+-- "URL: https://www.psc.edu/resources/software"
+-- "Category: Other"
+-- "Description: open-source document converter, widely used as a writing tool and as a basis for publishing workflows."
+-- "Keywords: singularity utilities"
+
+whatis("Name: pandoc")
+whatis("Version: 2.2.1")
+whatis("Category: Biological Sciences")
+whatis("URL: https://www.psc.edu/resources/software")
+whatis("open-source document converter, widely used as a writing tool and as a basis for publishing workflows. ")
+
+help([[
+pandoc 2.2.1
+-----------
+open-source document converter, widely used as a writing tool and as a basis for publishing workflows
+-----------
+To load the module, type
+> module load pandoc/2.2.1
+To unload the module, type
+> module unload pandoc/2.2.1
+Repository
+----------
+Tools included in this module are
+* pandoc
+]])
+
+local package = "pandoc"
+local version = "2.2.1"
+local base    = pathJoin("/opt/packages",package,version)
+prepend_path("PATH", base)
